062908008fbdf6052f5febcd0f6625c4daebb1c0
Guillermo Daniel Salazar <guillesalazar@gmail.com>
2020-06-02 10:49:17 -0300

    Adding new entity for current lending pool status.

--- src/utils/lendingpool-commons.ts
+++ src/utils/lendingpool-commons.ts
@@ -2,9 +2,11 @@ import { log, BigInt, ethereum } from "@graphprotocol/graph-ts";
 import {
   LendingPoolChange,
   EthTransaction,
+  LendingPoolStatus,
 } from "../../generated/schema";
 import { Address } from "@graphprotocol/graph-ts";
 import { buildId, createEthTransaction } from "./commons";
+import { LENDING_POOL_DEPOSITED, LENDING_POOL_REPAID, LENDING_POOL_WITHDRAWN, LENDING_POOL_LIQUIDATED, LENDING_POOL_INTEREST_WITHDRAWN } from "./consts";
 
 export function createLendingPoolChange(
   id: string,
@@ -37,6 +39,48 @@ export function createLendingPoolChange(
   daiPoolAction.save();
 }
 
+export function updateOrCreateLendingPoolStatus(
+  zToken: string,
+  lendingToken: string,
+  sum: boolean,
+  amount: BigInt,
+  transaction: EthTransaction
+): void {
+  log.info(
+    "Getting/Creating lending pool supply status for {} / {}",
+    [
+      zToken,
+      lendingToken,
+    ]
+  );
+  let id = zToken + "-" + lendingToken;
+  let entity = LendingPoolStatus.load(id)
+  if(entity == null) {
+    entity = new LendingPoolStatus(id)
+    entity.zToken = zToken
+    entity.lendingToken = lendingToken
+    entity.amount = BigInt.fromI32(0)
+  }
+  if(sum) {
+    entity.amount = entity.amount.plus(amount)
+  } else {
+    entity.amount = entity.amount.minus(amount)
+  }
+  entity.blockNumber = transaction.blockNumber;
+  entity.timestamp = transaction.timestamp;
+  entity.save();
+}
+
+export function getActionsMap():Map<string, boolean> {
+  let actionsMap = new Map<string, boolean>();
+  actionsMap.set(LENDING_POOL_DEPOSITED, true);
+  actionsMap.set(LENDING_POOL_REPAID, true);
+  actionsMap.set(LENDING_POOL_WITHDRAWN, false);
+  actionsMap.set(LENDING_POOL_LIQUIDATED, false);
+  actionsMap.set(LENDING_POOL_INTEREST_WITHDRAWN, false);
+  return actionsMap;
+}
+
 export function internalHandleLendingPoolChange(
   transactionType: string,
   zToken: string,
@@ -55,6 +99,15 @@ export function internalHandleLendingPoolChange(
     token,
     action,
     account,
+    amount,
+    ethTransaction
+  )
+  let actionsMap = getActionsMap();
+
+  updateOrCreateLendingPoolStatus(
+    zToken,
+    token,
+    actionsMap.get(action),
     amount,
     ethTransaction
   )
