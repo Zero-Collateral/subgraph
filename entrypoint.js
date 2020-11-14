27f1635701c0d5da96710273fc8c663d39650fda
Jeremi G <gendronjeremi@gmail.com>
2020-11-14 11:36:51 -0500

    up/entrypoint: deploy to ipfs only

--- entrypoint.js
+++ entrypoint.js
@@ -2,9 +2,9 @@ const { execSync } = require("child_process")
 
 async function entryPoint() {
   try {
-    execSync(`yarn build:$ETH_NETWORK && yarn graph deploy --node $GRAPH_NODE_URL --ipfs $IPFS_URL $SUBGRAPH_NAME`)
+    execSync(`yarn build:$ETH_NETWORK && yarn graph deploy --ipfs $IPFS_URL $SUBGRAPH_NAME`)
   } catch(e) {
-    console.log(e)
+    console.log(e.stderr, e.stdout)
     return entryPoint()
   }
 }
