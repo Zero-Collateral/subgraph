324b61f512908519123f3f7f8bd5db994cdc1284
Jeremi G <gendronjeremi@gmail.com>
2020-11-15 13:16:21 -0500

    rm: entrypoint.js

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
-
 yarn prepare:$ETH_NETWORK
 yarn codegen
 yarn build:$ETH_NETWORK
-node entrypoint.js
+yarn graph deploy --ipfs $IPFS_URL --node $NODE_URL $SUBGRAPH_NAME
