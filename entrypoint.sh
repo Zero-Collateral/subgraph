e7349f0fb1c4312caa5480144d5486bb175506da
Jeremi G <gendronjeremi@gmail.com>
2020-11-15 16:01:46 -0500

    up/entrypoint: no failure at create if subgraph name exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ set -e
 yarn prepare:$ETH_NETWORK
 yarn codegen
 yarn build:$ETH_NETWORK
-yarn graph create --node $NODE_URL $SUBGRAPH_NAME
-yarn graph deploy --ipfs $IPFS_URL --node $NODE_URL $SUBGRAPH_NAME
+yarn graph create --node $NODE_URL $SUBGRAPH_NAME || \
+  yarn graph deploy --ipfs $IPFS_URL --node $NODE_URL $SUBGRAPH_NAME
