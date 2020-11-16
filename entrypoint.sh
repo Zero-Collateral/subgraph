2ecfc9f9139a6046b95a08d14951eb2ee098f524
Jeremi G <gendronjeremi@gmail.com>
2020-11-15 21:04:23 -0500

    up/entrypoint: freeze to prevent container crash

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,9 @@ yarn prepare:$ETH_NETWORK
 yarn codegen
 yarn graph create --node $NODE_URL $SUBGRAPH_NAME && \
   yarn graph deploy --ipfs $IPFS_URL --node $NODE_URL $SUBGRAPH_NAME
+
+# Temporarily freezing this container
+while [ true ]
+do
+  sleep 3600
+done
