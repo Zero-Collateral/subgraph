048e5d4a53ca418081d015dcd07ce5c207397cbc
Jeremi G <gendronjeremi@gmail.com>
2020-11-15 19:38:26 -0500

    up/docker: remove number of times subgraph is compiled

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM node:14.15.0
 WORKDIR /app
 COPY . .
 RUN yarn
+RUN yarn codegen
 ENTRYPOINT [ "./entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 set -e
 
 yarn prepare:$ETH_NETWORK
-yarn codegen
-yarn build:$ETH_NETWORK
-yarn graph create --node $NODE_URL $SUBGRAPH_NAME || \
+
+yarn graph create --node $NODE_URL $SUBGRAPH_NAME && \
   yarn graph deploy --ipfs $IPFS_URL --node $NODE_URL $SUBGRAPH_NAME
