2c4790ca5fcffdd20f8aa8bfce4584f5c9456af9
Jeremi G <gendronjeremi@gmail.com>
2020-11-15 19:48:24 -0500

    rm: entrypoint; up: docker (run codegen at runtime)

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,4 @@ FROM node:14.15.0
 WORKDIR /app
 COPY . .
 RUN yarn
-RUN yarn codegen
 ENTRYPOINT [ "./entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 yarn prepare:$ETH_NETWORK
-
+yarn codegen
 yarn graph create --node $NODE_URL $SUBGRAPH_NAME && \
   yarn graph deploy --ipfs $IPFS_URL --node $NODE_URL $SUBGRAPH_NAME
