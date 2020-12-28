b0125bbc5ecd1f2f30480c075190310a58ac5953
Jeremi G <gendronjeremi@gmail.com>
2020-12-27 21:13:14 -0500

    up/entrypoint: update existing subgraph if it exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,12 @@ set -e
 
 yarn prepare:$ETH_NETWORK
 yarn codegen
-yarn graph create --node $NODE_URL $SUBGRAPH_NAME && \
+
+if yarn graph create --node $NODE_URL $SUBGRAPH_NAME; then
   yarn graph deploy --ipfs $IPFS_URL --node $NODE_URL $SUBGRAPH_NAME
+else
+  yarn graph deploy --ipfs $IPFS_URL --node $NODE_URL $SUBGRAPH_NAME
+fi
 
 # Temporarily freezing this container
 while [ true ]
