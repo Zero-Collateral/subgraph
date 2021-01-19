99bc134db2ad4970e9ad826cce9bbf26cab20458
Jeremi G <gendronjeremi@gmail.com>
2021-01-19 16:51:50 -0500

    up: use new AssetSettings mapping/abi instead of SettingsInterface

---
+++ src/mappings/assetSettingsMappings.ts
@@ -0,0 +1,86 @@
+import { Bytes, BigInt } from "@graphprotocol/graph-ts"
+import { createAssetSettingsChange, getOrCreateAssetSettingsStatus, updateAddressAssetSettingsStatus, updateBigIntAssetSettingsStatus } from "../utils/common-settings"
+import { ASSET_SETTINGS_CTOKEN_ADDRESS, ASSET_SETTINGS_MAX_LOAN_AMOUNT, ASSET_SETTINGS_REMOVED } from "../utils/consts"
+import {
+  AssetSettingsCreated as AssetSettingsCreatedEvent,
+  AssetSettingsUintUpdated as AssetSettingsUintUpdatedEvent,
+  AssetSettingsAddressUpdated as AssetSettingsAddressUpdatedEvent,
+  AssetSettingsRemoved as AssetSettingsRemovedEvent,
+} from "../../generated/AssetSettingsInterface/AssetSettingsInterface";
+
+export function handleAssetSettingsCreated(event: AssetSettingsCreatedEvent): void {
+  createAssetSettingsChange(
+    event.params.sender,
+    event.params.assetAddress,
+    Bytes.fromUTF8(ASSET_SETTINGS_CTOKEN_ADDRESS) as Bytes,
+    '',
+    event.params.cTokenAddress.toHexString(),
+    event
+  )
+
+  createAssetSettingsChange(
+    event.params.sender,
+    event.params.assetAddress,
+    Bytes.fromUTF8(ASSET_SETTINGS_MAX_LOAN_AMOUNT) as Bytes,
+    '0',
+    event.params.maxLoanAmount.toString(),
+    event
+  )
+  getOrCreateAssetSettingsStatus(
+    event.params.assetAddress,
+    event.params.cTokenAddress,
+    event.params.maxLoanAmount,
+    event
+  )
+}
+
+export function handleAssetSettingsUintUpdated(event: AssetSettingsUintUpdatedEvent): void {
+  createAssetSettingsChange(
+    event.params.sender,
+    event.params.assetAddress,
+    event.params.assetSettingName,
+    event.params.oldValue.toString(),
+    event.params.newValue.toString(),
+    event
+  )
+  updateBigIntAssetSettingsStatus(
+    event.params.assetAddress,
+    event.params.assetSettingName.toString(),
+    event.params.newValue,
+    event
+  )
+}
+
+export function handleAssetSettingsAddressUpdated(event: AssetSettingsAddressUpdatedEvent): void {
+  createAssetSettingsChange(
+    event.params.sender,
+    event.params.assetAddress,
+    event.params.assetSettingName,
+    event.params.oldValue.toHexString(),
+    event.params.newValue.toHexString(),
+    event
+  )
+  updateAddressAssetSettingsStatus(
+    event.params.assetAddress,
+    event.params.assetSettingName.toString(),
+    event.params.newValue,
+    event
+  )
+}
+
+export function handleAssetSettingsRemoved(event: AssetSettingsRemovedEvent): void {
+  createAssetSettingsChange(
+    event.params.sender,
+    event.params.assetAddress,
+    Bytes.fromUTF8(ASSET_SETTINGS_REMOVED) as Bytes,
+    'false',
+    'true',
+    event
+  )
+  updateBigIntAssetSettingsStatus(
+    event.params.assetAddress,
+    ASSET_SETTINGS_REMOVED,
+    BigInt.fromI32(0),
+    event
+  )
+}
--- src/mappings/settingsMappings.ts
+++ src/mappings/settingsMappings.ts
@@ -9,10 +9,6 @@ import {
   Unpaused as UnpausedEvent,
   PauserAdded as PauserAddedEvent,
   PauserRemoved as PauserRemovedEvent,
-  AssetSettingsCreated as AssetSettingsCreatedEvent,
-  AssetSettingsUintUpdated as AssetSettingsUintUpdatedEvent,
-  AssetSettingsAddressUpdated as AssetSettingsAddressUpdatedEvent,
-  AssetSettingsRemoved as AssetSettingsRemovedEvent,
 } from "../../generated/SettingsInterface/SettingsInterface";
 
 import { getTimestampInMillis } from "../utils/commons";
@@ -187,81 +183,4 @@ export function handlePauserRemoved(event: PauserRemovedEvent): void {
     event
   );
   updateOrCreatePauserStatus(event.params.account, false, event);
-}
-
-export function handleAssetSettingsCreated(event: AssetSettingsCreatedEvent): void {
-  createAssetSettingsChange(
-    event.params.sender,
-    event.params.assetAddress,
-    Bytes.fromUTF8(ASSET_SETTINGS_CTOKEN_ADDRESS) as Bytes,
-    '',
-    event.params.cTokenAddress.toHexString(),
-    event
-  )
-
-  createAssetSettingsChange(
-    event.params.sender,
-    event.params.assetAddress,
-    Bytes.fromUTF8(ASSET_SETTINGS_MAX_LOAN_AMOUNT) as Bytes,
-    '0',
-    event.params.maxLoanAmount.toString(),
-    event
-  )
-  getOrCreateAssetSettingsStatus(
-    event.params.assetAddress,
-    event.params.cTokenAddress,
-    event.params.maxLoanAmount,
-    event
-  )
-}
-
-export function handleAssetSettingsUintUpdated(event: AssetSettingsUintUpdatedEvent): void {
-  createAssetSettingsChange(
-    event.params.sender,
-    event.params.assetAddress,
-    event.params.assetSettingName,
-    event.params.oldValue.toString(),
-    event.params.newValue.toString(),
-    event
-  )
-  updateBigIntAssetSettingsStatus(
-    event.params.assetAddress,
-    event.params.assetSettingName.toString(),
-    event.params.newValue,
-    event
-  )
-}
-
-export function handleAssetSettingsAddressUpdated(event: AssetSettingsAddressUpdatedEvent): void {
-  createAssetSettingsChange(
-    event.params.sender,
-    event.params.assetAddress,
-    event.params.assetSettingName,
-    event.params.oldValue.toHexString(),
-    event.params.newValue.toHexString(),
-    event
-  )
-  updateAddressAssetSettingsStatus(
-    event.params.assetAddress,
-    event.params.assetSettingName.toString(),
-    event.params.newValue,
-    event
-  )
-}
-
-export function handleAssetSettingsRemoved(event: AssetSettingsRemovedEvent): void {
-  createAssetSettingsChange(
-    event.params.sender,
-    event.params.assetAddress,
-    Bytes.fromUTF8(ASSET_SETTINGS_REMOVED) as Bytes,
-    'false',
-    'true',
-    event
-  )
-  updateBigIntAssetSettingsStatus(
-    event.params.assetAddress,
-    ASSET_SETTINGS_REMOVED,
-    BigInt.fromI32(0),
-    event
-  )
 }
